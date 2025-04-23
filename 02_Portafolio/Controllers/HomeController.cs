574acef50b95f9e60d12d453771fbd7d0e01de53
Jairo Yate <jyatem@gmail.com>
2025-04-23 06:42:36 -0500

    Ok

--- 02_Portafolio/Controllers/HomeController.cs
+++ 02_Portafolio/Controllers/HomeController.cs
@@ -15,17 +15,74 @@ namespace _02_Portafolio.Controllers
 
         public IActionResult Index()
         {
+            #region Codigo Comentado
             //ViewBag.Nombre = "Jairo Yesid Yate";
             //ViewBag.Edad = 44;
             //return View("Index", "Jairo Yate Martinez");
 
-            var persona = new Persona()
-            {
-                Nombre = "Lina Mora",
-                Edad = 14
-            };
+            //var persona = new Persona()
+            //{
+            //    Nombre = "Lina Mora",
+            //    Edad = 14
+            //};
+
+            //return View(persona); 
+            #endregion
+
+            var listadoProyectos = ObtenerProyectos().Take(3).ToList();
             
-            return View(persona);
+            var homeIndexViewModel = new HomeIndexViewModel() { Proyectos = listadoProyectos };
+
+            return View(homeIndexViewModel);
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+            #region Agregando elementos a una lista con el método Add
+            //var listadoProyecto = new List<Proyecto>();
+
+            //listadoProyecto.Add(new Proyecto()
+            //{
+            //    Titulo = "Amazon",
+            //    Descripcion = "E-Commerce realizado en ASP.NET Core",
+            //    Link = "https://amazon.com",
+            //    ImageURL = ""
+            //}); 
+            #endregion
+
+            var listadoProyecto = new List<Proyecto>()
+            {
+                new Proyecto()
+                {
+                    Titulo = "Amazon",
+                    Descripcion = "E-Commerce realizado en ASP.NET Core",
+                    Link = "https://amazon.com",
+                    ImageURL = "/imagenes/amazon.png"
+                },
+                new Proyecto()
+                {
+                    Titulo = "New York Times",
+                    Descripcion = "Página de noticias en React",
+                    Link = "https://nytimes.com",
+                    ImageURL = "/imagenes/nyt.png"
+                },
+                new Proyecto()
+                {
+                    Titulo = "Reddit",
+                    Descripcion = "Red social para compartir en comunidades",
+                    Link = "https://reddit.com",
+                    ImageURL = "/imagenes/reddit.png"
+                },
+                new Proyecto()
+                {
+                    Titulo = "Steam",
+                    Descripcion = "Tienda en línea para comprar videojuegos",
+                    Link = "https://store.steampowered.com",
+                    ImageURL = "/imagenes/steam.png"
+                },
+            };
+
+            return listadoProyecto;
         }
 
         public IActionResult Privacy()
