a07b8e5da914635fa6b68df0dd548cbe828ed7ef
Jairo Yate <jyatem@gmail.com>
2025-04-30 07:30:22 -0500

    Ok

--- 02_Portafolio/Controllers/HomeController.cs
+++ 02_Portafolio/Controllers/HomeController.cs
@@ -9,11 +9,13 @@ namespace _02_Portafolio.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IRepositorioProyectos repositorioProyectos;
+        private readonly IConfiguration configuration;
 
-        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos)
+        public HomeController(ILogger<HomeController> logger, IRepositorioProyectos repositorioProyectos, IConfiguration configuration)
         {
             _logger = logger;
             this.repositorioProyectos = repositorioProyectos;
+            this.configuration = configuration;
         }
 
         public IActionResult Index()
@@ -32,17 +34,42 @@ namespace _02_Portafolio.Controllers
             //return View(persona); 
             #endregion
 
-            _logger.LogTrace("Este es un mensaje de trace");
-            _logger.LogDebug("Este es un mensaje de debug");
-            _logger.LogInformation("Este es un mensaje de information");
-            _logger.LogWarning("Este es un mensaje de warning");
-            _logger.LogError("Este es un mensaje de error");
-            _logger.LogCritical("Este es un mensaje de critical");
+            #region Manejo de Logs y variables de configuración
+            //var apellido = configuration.GetValue<string>("Apellido");
+            //_logger.LogTrace("Este es un mensaje de trace");
+            //_logger.LogDebug("Este es un mensaje de debug");
+            //_logger.LogInformation("Este es un mensaje de information");
+            //_logger.LogWarning("Este es un mensaje de warning");
+            //_logger.LogError("Este es un mensaje de error");
+            //_logger.LogCritical("Este es un mensaje de critical " + apellido); 
+            #endregion
 
             var listadoProyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();            
             var homeIndexViewModel = new HomeIndexViewModel() { Proyectos = listadoProyectos };
 
             return View(homeIndexViewModel);
+        }
+
+        public IActionResult Proyectos()
+        {
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
+        {
+            return RedirectToAction("MensajeExitoso");
+        }
+
+        public IActionResult MensajeExitoso()
+        {
+            return View();
         }
 
         public IActionResult Privacy()
---
+++ 02_Portafolio/Servicios/ServicioEmailGmail.cs
@@ -0,0 +1,14 @@
+﻿namespace _02_Portafolio.Servicios
+{
+    public class ServicioEmailGmail
+    {
+        private readonly IConfiguration configuration;
+
+        public ServicioEmailGmail(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+
+    }
+}
